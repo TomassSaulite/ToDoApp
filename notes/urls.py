f550bac66b7f9bc5369510a66e235c8c5ae09e93
Томас Саулишкалнс <tomassauliskalns@Tomass-MacBook-Pro.local>
2025-07-31 16:53:54 +0300

    added complete and remove button to each task

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.note_list, name='note_list'),
     path('add/', views.add_note, name='add_note'),
+    path('complete/<int:note_id>/', views.mark_completed, name='mark_completed'),
+    path('remove/<int:note_id>/', views.remove_task, name='remove_task'),
+
+
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,18 +1,39 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from .models import Note
 from django.contrib.auth.decorators import login_required
 
+
+def mark_completed(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    note.is_completed = True
+    note.save()
+    print(f"Marked as completed: {note.title} → {note.is_completed}")
+    return redirect('note_list')
+
+def remove_task(request, note_id):
+    note = get_object_or_404(Note, id=note_id)
+    print(f"Task removed: {note.title}")
+    note.delete()
+    return redirect('note_list')
+
+
 @login_required
 def note_list(request):
-    notes = Note.objects.filter(user=request.user).order_by('-created_at')
+    notes = Note.objects.filter(user=request.user).order_by('-is_completed', '-created_at')
     return render(request, 'notes/note_list.html', {'notes': notes})
+
+
+
+
+
+
+
 
 @login_required
 def add_note(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
-
         if title and content:
             Note.objects.create(user=request.user, title=title, content=content)
             return redirect('note_list')
